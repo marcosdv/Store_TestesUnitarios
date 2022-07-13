31dc857f4286e4038d77e422b9bbccdfe8ef6a4c
marcosdv <marcosdiasvendramini@yahoo.com.br>
2022-07-12 23:35:59 -0300

    Adicionar arquivos de projeto.

---
+++ Store.Domain/Entities/Custumer.cs
@@ -0,0 +1,13 @@
+﻿namespace Store.Domain.Entities;
+
+public class Custumer : Entity
+{
+    public Custumer(string name, string email)
+    {
+        Name = name;
+        Email = email;
+    }
+
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+}
---
+++ Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,12 @@
+﻿namespace Store.Tests.Entities;
+
+[TestClass]
+public class OrderTests
+{
+    [TestMethod]
+    [TestCategory("Domain")]
+    public void Dado_um_novo_pedido_valido_ele_deve_gerar_um_numero_com_8_caracteres()
+    {
+        Assert.Fail();
+    }
+}
